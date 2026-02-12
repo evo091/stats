30fb28eee02425191cc68058f35da322f9e75fec
Ian <236912195+evo091@users.noreply.github.com>
2026-02-12 00:19:52 -0500

    completed getEvens

--- index.js
+++ index.js
@@ -77,8 +77,13 @@ function getRange(numbers) {
 function getEvens(numbers) {
   let evens = [];
   for (let i = 0; i < numbers.length; ++i) {
-    if 
+    if ((numbers[i] % 2) === 0) {
+      evens.push(numbers[i]);
+    }
+  }
+  return evens;
 }
+
 
 /**
  * @param {number[]} numbers an array of integers
